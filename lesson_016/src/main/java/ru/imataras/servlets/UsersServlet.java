eb7c2c82a7b5e09bf6847fd63cbcc46695323f17
Ivan <imataras@yandex.ru>
2017-03-17 02:09:38 +0300

    lesson 016 edit

--- lesson_016/src/main/java/ru/imataras/servlets/UsersServlet.java
+++ lesson_016/src/main/java/ru/imataras/servlets/UsersServlet.java
@@ -1,8 +1,6 @@
 package ru.imataras.servlets;
 
 import org.slf4j.Logger;
-import ru.imataras.models.User;
-import ru.imataras.storages.MessageStorage;
 import ru.imataras.storages.RoleStorage;
 import ru.imataras.storages.UserStorage;
 
@@ -10,7 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -24,17 +21,19 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class UsersServlet extends HttpServlet {
 
-    private static final Logger log = getLogger(UserServlet.class);
+    private static final Logger log = getLogger(UsersServlet.class);
     private final RoleStorage roles = RoleStorage.getInstance();
     private final UserStorage users = UserStorage.getInstance();
-    private final MessageStorage messages = MessageStorage.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = this.users.findById(Integer.valueOf(req.getParameter("id"))).get();
-        req.setAttribute("user", user);
-        req.setAttribute("messages", this.messages.findByOwner(user.getId()));
-        req.getRequestDispatcher("/WEB-INF/views/users/user.jsp").forward(req, resp);
+        req.setAttribute("users", this.users.getAll());
+        req.getRequestDispatcher("/WEB-INF/views/users/users.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
     }
 
 }
